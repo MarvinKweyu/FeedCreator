6c1672f1f6fd9c585ea21ca9aeb1a0ca6870a692
MarvinKweyu <mkweyu1@gmail.com>
2019-10-20 19:00:37 +0300

    Refactor static files.Fix static file url

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.sitemaps',
     'django.contrib.postgres', # allow text searching
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,12 +18,16 @@ from django.urls import path,include
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
 
+from django.views.generic.base import TemplateView
+
+
 sitemaps = {
     'posts':PostSitemap,
 }
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='blog/index.html'), name='home'),
     path('blog/',include('blog.urls',namespace='blog')),
     path('sitemap.xml',sitemap, {'sitemaps':sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
